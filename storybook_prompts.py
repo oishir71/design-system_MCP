ad6b35dc78cccc5d45de08da18f08e7a0f724f73
Reiyo OISHI <reiyo.oishi@g.softbank.co.jp>
2025-05-08 12:38:57 +0900

    Update prompts and server

--- storybook_prompts.py
+++ storybook_prompts.py
@@ -2,19 +2,36 @@ from mcp.types import Prompt, PromptArgument
 
 prompts: list[Prompt] = [
     Prompt(
-        name="design-system_storybook",
-        description="Generate a description of design-system",
+        name="softreef_overview",
+        description="Generate a description of design-system overview",
         arguments=[
             PromptArgument(
                 name="category",
                 description="type of major category",
                 required=True,
-            ),
+            )
+        ],
+    ),
+    Prompt(
+        name="softreef_component",
+        description="Generate a description of design-system component",
+        arguments=[
             PromptArgument(
                 name="component",
                 description="type of component",
-                required=False,
-            ),
+                required=True,
+            )
         ],
-    )
+    ),
+    Prompt(
+        name="softreef_design_pattern",
+        description="Generate a description of design-pattern",
+        arguments=[
+            PromptArgument(
+                name="pattern",
+                description="type of design-pattern",
+                required=True,
+            )
+        ],
+    ),
 ]
